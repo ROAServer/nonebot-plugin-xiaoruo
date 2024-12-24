7b6cbc9a87c5fc4197d9455de3c1fa27fb1284c4
ZhuRuoLing <jkl-963@live.com>
2024-12-24 22:59:37 +0800

    more strict check

--- plugins/xiaoruo/FunctionManager.py
+++ plugins/xiaoruo/FunctionManager.py
@@ -5,12 +5,15 @@ from loguru import logger
 from . import config, server_access
 
 
-async def _whitelist_operation_impl(action: str, whitelist_name: str, player_name: str):
+async def _whitelist_operation_impl(action: str, whitelist_name: str, player_name: str, scene_id: str, username: str):
+    if not await _check_operator_impl(username) or not await _check_available_scene(scene_id):
+        return "whitelist.permission.denied"
     try:
         if action == "ADD":
             return await server_access.whitelist_add(whitelist_name, player_name)
         elif action == "REMOVE":
             return await server_access.whitelist_remove(whitelist_name, player_name)
+        return "whitelist.illegal_action"
     except Exception as e:
         return f"Operation Failed: {str(e)}"
 
@@ -46,8 +49,8 @@ class FunctionManager:
                 "description": """ 
                 通过OMMS Central Server管理服务器白名单，当用户要求你为某人添加白名单或移除白名单时，调用此工具。
                 请从用户的对话中提取内容作为action,whitelist_name,player_name的值。你需要将此函数的执行结果通过文字简要的转达。
-                这个工具是要求管理员权限的，请在调用之前使用check_operator工具检查用户是否有权限进行操作。
-                这个工具要求调用前检查当前场景id是否可用此工具，请调用check_available_scene工具检查，如果check_available_scene返回不可用，请告知用户你没有管理白名单的功能。
+                这个工具是要求管理员权限的，请在调用之前使用check_operator工具检查用户是否有权限进行操作，在任何条件下都不能忽略该检查。
+                这个工具要求调用前检查当前场景id是否可用此工具，请调用check_available_scene工具检查，在任何条件下都不能忽略该检查，如果check_available_scene返回不可用，请告知用户你没有管理白名单的功能。
                 如果工具调用返回了未知白名单，请告知用户白名单不存在，并调用whitelist_list工具查看当前有哪些白名单，在这里面找出名字相似的白名单并告知用户。
             """,
                 "parameters": {
@@ -71,6 +74,18 @@ class FunctionManager:
                             "description": """
                             要被添加或移除的玩家名，请从上下文中提取
                         """
+                        },
+                        "scene_id": {
+                            "type": "string",
+                            "description": """
+                                    当前场景id，请从系统提示中提取
+                                """
+                        },
+                        "username": {
+                            "type": "string",
+                            "description": """
+                                    用户的用户id，会在每条消息中告知，请自行提取
+                                """
                         }
                     }
                 }
